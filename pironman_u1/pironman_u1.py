61b78c2e606955064cc7a2ec97d6c8a12cd5ea70
Lovmong <1967471283@qq.com>
2024-06-19 09:34:18 +0800

    change SPC init

--- pironman_u1/pironman_u1.py
+++ pironman_u1/pironman_u1.py
@@ -2,7 +2,7 @@ from spc.spc import SPC
 
 class PironmanU1(SPC):
     def __init__(self):
-        super().__init__(mode='esp32')
+        super().__init__()
 
     def set_fan_mode(self, mode):
         self.write_fan_mode(mode)
